04884139548e32dd9001e8a9c0a7a78923d3a1ef
cheesemonki <myj18007718546@163.com>
2024-07-29 21:42:23 +0800

    初始化AHT10传感器并优化数据读取流程
    对AHT10传感器的初始化代码进行了调整，并优化了温度和湿度的读取流程。删除了原本在读取前的传感器初始化步骤，现在仅在传感器状态异常时重新进行初始化。同时，对读取数据时的错误处理进行了改进，增加了对无效数据的重试机制，确保在数据读取失败时能够尝试重新获取，提高了代码的健壮性。
    
    此外，代码中还移除了未使用的`calibration_status`变量，并对`is_calibration_enabled`方法进行了格式调整，以符合Python的代码风格指南。

--- lib/aht10_drivers/aht10_driver.py
+++ lib/aht10_drivers/aht10_driver.py
@@ -14,57 +14,55 @@ class AHT10:
         self.address = address
 
     def sensor_init(self):
-    buf=bytearray(2)
-    buf[0] = 0x00
-    buf[1] = 0x00
-    self.i2c.writeto_mem(self.address, AHT10_NORMAL_CMD, buf)
-    time.sleep_ms(350)
-    buf[0] = 0x08
-    buf[1] = 0x00
-    self.i2c.writeto_mem(self.address, AHT10_CALIBRATION_CMD, buf)
-    time.sleep_ms(450)
-    calibration_status= self.i2c.readfrom(self.address, 1)  # 读取状态字节
-    print("AHT10 calibration status:", calibration_status)  # 打印状态字节
+        buf=bytearray(2)
+        buf[0] = 0x00
+        buf[1] = 0x00
+        self.i2c.writeto_mem(self.address, AHT10_NORMAL_CMD, buf)
+        time.sleep_ms(350)
+        buf[0] = 0x08
+        buf[1] = 0x00
+        self.i2c.writeto_mem(self.address, AHT10_CALIBRATION_CMD, buf)
+        time.sleep_ms(450)
 
-def is_calibration_enabled(self):
-    status = self.i2c.readfrom(self.address, 1)
-    status_hex = struct.unpack_from(">b", status)
-    if status_hex[0] & int('0x68', 16) == int('0x08', 16):
-        return True
-    else:
-        return False
+    def is_calibration_enabled(self):
+        status = self.i2c.readfrom(self.address, 1)
+        status_hex = struct.unpack_from(">b", status)
+        if status_hex[0] & int('0x68', 16) == int('0x08', 16):
+            return True
+        else:
+            return False
 
-def read_temperature(self):
-    cmd=bytearray(2)
-    cmd[0] = 0x00
-    cmd[1] = 0x00
-    self.i2c.writeto_mem(self.address, AHT10_GET_DATA, cmd)
+    def read_temperature(self):
+        cmd=bytearray(2)
+        cmd[0] = 0x00
+        cmd[1] = 0x00
+        self.i2c.writeto_mem(self.address, AHT10_GET_DATA, cmd)
 
-    if self.is_calibration_enabled():
-        temp = self.i2c.readfrom(self.address, 6)
-        temp_hex = struct.unpack(">BBBBBB", temp)
-        cur_temp = ((temp_hex[3] & 0xf) << 16 | temp_hex[4] << 8 | temp_hex[5]) * 200.0 / (1 << 20) - 50
-        return cur_temp
-    else:
-        self.sensor_init()
-        print("The aht10 is under an abnormal status. Please try again")
-
-def read_humidity(self):
-    cmd=bytearray(2)
-    cmd[0] = 0x00
-    cmd[1] = 0x00
-    self.i2c.writeto_mem(self.address, AHT10_GET_DATA, cmd)
-
-    if self.is_calibration_enabled():
-        temp = self.i2c.readfrom(self.address, 6)
-        temp_hex = struct.unpack(">BBBBBB", temp)
-
-        while temp_hex[2] == 0:
-            print("Data reading failed. Trying again...")
+        if self.is_calibration_enabled():
             temp = self.i2c.readfrom(self.address, 6)
             temp_hex = struct.unpack(">BBBBBB", temp)
-        cur_humi = (temp_hex[1] << 12 | temp_hex[2] << 4 | (temp_hex[3] & 0xf0) >> 4) * 100.0 / (1 << 20)
-        return cur_humi
-    else:
-        self.sensor_init()
-        print("The aht10 is under an abnormal status. Please try again")
+            cur_temp = ((temp_hex[3] & 0xf) << 16 | temp_hex[4] << 8 | temp_hex[5]) * 200.0 / (1 << 20) - 50
+            return cur_temp
+        else:
+            self.sensor_init()
+            print("The aht10 is under an abnormal status. Please try again")
+
+    def read_humidity(self):
+        cmd=bytearray(2)
+        cmd[0] = 0x00
+        cmd[1] = 0x00
+        self.i2c.writeto_mem(self.address, AHT10_GET_DATA, cmd)
+
+        if self.is_calibration_enabled():
+            temp = self.i2c.readfrom(self.address, 6)
+            temp_hex = struct.unpack(">BBBBBB", temp)
+
+            while temp_hex[2] == 0:
+                temp = self.i2c.readfrom(self.address, 6)
+                temp_hex = struct.unpack(">BBBBBB", temp)
+
+            cur_humi = (temp_hex[1] << 12 | temp_hex[2] << 4 | (temp_hex[3] & 0xf0) >> 4) * 100.0 / (1 << 20)
+            return cur_humi
+        else:
+            self.sensor_init()
+            print("The aht10 is under an abnormal status. Please try again")
